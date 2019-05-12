20107ece030f6f00612d05913ea0b837b78bbb61
NKornienko <50046666+NKornienko@users.noreply.github.com>
2019-05-12 19:37:44 +0300

    Расстояние между точками. Метод

--- sandbox/src/main/java/ru/stqa/pft/sandbox/My_Сalculation_task2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/My_Сalculation_task2.java
@@ -4,16 +4,9 @@ public class My_Сalculation_task2 {
    public static void main(String[] args) {
 
       Point a = new Point(5.7,3.0);
-      Point b = new Point(2.1,1.1);
+
       System.out.println ("точка A имеет координаты :  x = " +  a.x + " и  y = " +  a.y  );
-      System.out.println ("точка B имеет координаты :  x = " +  b.x + " и  y = " +  b.y);
-
-      System.out.println ("Расстояние между точками А и В   = " + distance(a,b));
+      
+      System.out.println ("Расстояние между точками А и В   = " + a.distance(2.0,5.6));
    }
-
-public static double distance(Point a,Point b){
-      return Math.sqrt((a.x -b.x)*(a.x -b.x)+(a.y -b.y)*(a.y -b.y));
-
 }
-}
-
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -3,9 +3,20 @@ package ru.stqa.pft.sandbox;
 public class Point {
    public double x;
    public double y;
+   private double х1;
+   private double у1;
 
    public Point(double x, double y) {
       this.x = x;
       this.y = y;
    }
+
+    public double distance (double x1, double y1)
+   {
+      double sx = (this.x - х1);
+      double sy = (this.y - у1);
+      return Math.sqrt(sx*sx + sy*sy);
+   }
+
+
 }
