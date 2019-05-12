eb38f3d0295b6eac223d6b7b59015a9eaabf4004
NKornienko <50046666+NKornienko@users.noreply.github.com>
2019-05-12 12:33:41 +0300

    Создан пакет для моей первой программы

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgramm {
+
+    public static void main(String[] args) {
+
+
+        hello();
+    }
+
+    public static void hello() {
+
+        String somebody = "world"
+        System.out.println("Hello,world!");
+    }
+}
+
+
