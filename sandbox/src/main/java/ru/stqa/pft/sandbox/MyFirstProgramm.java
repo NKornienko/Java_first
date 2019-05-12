6e991927ee8bf83300e2fec33a11781f7233ea02
NKornienko <50046666+NKornienko@users.noreply.github.com>
2019-05-12 15:39:05 +0300

    Вычисления площадей квадрата и прямоугольника

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -8,12 +8,11 @@ public class MyFirstProgramm {
       hello("user");
       hello("Nady");
 
-      double l = 5;
-      System.out.println("площадь квадрата со стороной " + l + " равна " + area(l));
+      Square s = new Square(5.7);
+      System.out.println("площадь квадрата со стороной " + s.l + " равна " + s.area());
 
-      double a = 4;
-      double b = 7.3;
-      System.out.println("площадь прямоугольника со сторонами " + a + " b " + b + " = " + area(a, b));
+      Rectangle r = new Rectangle(4,7.3);
+      System.out.println("площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
    }
 
    public static void hello(String somebody) {
@@ -21,14 +20,7 @@ public class MyFirstProgramm {
       System.out.println("Hello," + somebody + "!");
    }
 
-   public static double area(double len) {
 
-      return len * len;
-   }
-
-   public static double area(double a, double b) {
-      return a * b;
-   }
 }
 
 
